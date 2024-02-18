709522d79a0c5d6a80ef242152aa346f1c9870ce
KieVitz <s217442@sggw.edu.pl>
2024-02-18 19:46:59 +0100

    Restructured commands

--- src/main/java/me/stahu/gsblockshuffle/commands/BlockShuffleCommand.java
+++ src/main/java/me/stahu/gsblockshuffle/commands/BlockShuffleCommand.java
@@ -1,18 +1,17 @@
 package me.stahu.gsblockshuffle.commands;
 
 import me.stahu.gsblockshuffle.GSBlockShuffle;
+import me.stahu.gsblockshuffle.commands.subcommands.DebugSubcommand;
+import me.stahu.gsblockshuffle.commands.subcommands.TeamSubcommand;
 import me.stahu.gsblockshuffle.event.GameStateManager;
 import me.stahu.gsblockshuffle.gui.page.GuiPage;
 import me.stahu.gsblockshuffle.team.TeamsManager;
 import org.bukkit.ChatColor;
-import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-
-import java.util.ArrayList;
 
 public class BlockShuffleCommand implements CommandExecutor {
     private final GuiPage categorySelectionGui;
@@ -20,7 +19,8 @@ public class BlockShuffleCommand implements CommandExecutor {
     private final YamlConfiguration settings;
     private final GSBlockShuffle plugin;
     private final TeamsManager teamManager;
-    private final ArrayList<CommandSender> debugModeUsers = new ArrayList<>();
+    private final DebugSubcommand debugSubcommand;
+    private final TeamSubcommand teamSubcommand;
 
     public BlockShuffleCommand(GuiPage categorySelectionGui, GameStateManager gameStateManager, YamlConfiguration settings, GSBlockShuffle plugin, TeamsManager teamManager) {
         this.categorySelectionGui = categorySelectionGui;
@@ -28,6 +28,8 @@ public class BlockShuffleCommand implements CommandExecutor {
         this.settings = settings;
         this.plugin = plugin;
         this.teamManager = teamManager;
+        this.debugSubcommand = new DebugSubcommand(plugin, gameStateManager, settings);
+        this.teamSubcommand = new TeamSubcommand(plugin, gameStateManager, settings, teamManager);
     }
 
     @Override
@@ -44,113 +46,25 @@ public class BlockShuffleCommand implements CommandExecutor {
             if (args.length == 0) {
                 this.categorySelectionGui.open(player);
             }
-            if (args[0].equalsIgnoreCase("debug")) {
-                onDebugCommand(sender, command, label, args);
-            }
-            if (args[0].equalsIgnoreCase("start")) {
-                if (!gameStateManager.setGameState(1)) {
-                    player.sendMessage(ChatColor.RED + "Game is already running.");
-                }
-            }
-            if (args[0].equalsIgnoreCase("end")) {
-                if (!gameStateManager.setGameState(0)) {
-                    player.sendMessage(ChatColor.RED + "Game is already stopped.");
-                }
-            }
-            if (args[0].equalsIgnoreCase("team")) {
-                onTeamCommand(sender, command, label, args);
+            switch (args[0].toLowerCase()) {
+                case "debug" -> debugSubcommand.parseSubcommand(sender, command, label, args);
+                case "team" -> teamSubcommand.parseSubcommand(sender, command, label, args);
+                case "start" -> startGame(player);
+                case "end" -> endGame(player);
             }
         }
         return true;
     }
 
-    private void onTeamCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length == 1) {
-            sender.sendMessage(ChatColor.RED + "Your team is: " + ChatColor.DARK_AQUA + teamManager.getPlayerTeam((Player) sender));
-            return;
+    private void startGame(Player player) {
+        if (!gameStateManager.setGameState(1)) {
+            player.sendMessage(ChatColor.RED + "Game is already running.");
         }
     }
 
-    private void onDebugCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (!debugModeUsers.contains(sender)) {
-            sender.sendMessage(ChatColor.RED + "WARNING! Debug mode is not supported and may cause the plugin to behave non-deterministically.");
-            debugModeUsers.add(sender);
+    private void endGame(Player player) {
+        if (!gameStateManager.setGameState(0)) {
+            player.sendMessage(ChatColor.RED + "Game is already stopped.");
         }
-        if (args.length == 1) {
-            for (String key : settings.getKeys(false)) {
-                sender.sendMessage(ChatColor.DARK_AQUA + key + ChatColor.WHITE + ": " + ChatColor.DARK_GREEN + settings.get(key));
-            }
-            return;
-        }
-        if (args.length < 2) {
-            return;
-        }
-        if (args[1].equalsIgnoreCase("saveSettings")) {
-            try {
-                plugin.saveSettings(settings);
-                sender.sendMessage(ChatColor.GREEN + "Successfully saved settings.");
-            } catch (Exception e) {
-                System.out.println(e);
-                sender.sendMessage(ChatColor.RED + "Failed to save settings.");
-            }
-            return;
-        }
-        if (args[1].equalsIgnoreCase("playTing")) {
-            sender.sendMessage(ChatColor.GREEN + "Playing ting sound.");
-            Player player = (Player) sender;
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1, 0.5F);
-            player.playSound(player.getLocation(), Sound.BLOCK_NOTE_BLOCK_CHIME, 1, 1F);
-            return;
-        }
-        if (args[1].equalsIgnoreCase("startGame")) {
-            sender.sendMessage(ChatColor.GREEN + "Game start invoked.");
-            gameStateManager.setGameState(1);
-            return;
-        }
-        if (args[1].equalsIgnoreCase("endGame")) {
-            sender.sendMessage(ChatColor.GREEN + "Game end invoked");
-            gameStateManager.setGameState(0);
-            return;
-        }
-        if (args[1].equalsIgnoreCase("newRound")) {
-            sender.sendMessage(ChatColor.GREEN + "New round invoked.");
-            gameStateManager.newRound();
-            return;
-        }
-        if (args[1].equalsIgnoreCase("endRound")) {
-            sender.sendMessage(ChatColor.GREEN + "End round invoked.");
-            gameStateManager.endRound();
-            return;
-        }
-        if (args[1].equalsIgnoreCase("getRoundsRemaining")) {
-            sender.sendMessage(ChatColor.GREEN + "Rounds remaining: " + ChatColor.DARK_AQUA + gameStateManager.getRoundsRemaining());
-            return;
-        }
-        if (args[1].equalsIgnoreCase("get")) {
-            String key = args[2];
-            if (!settings.contains(key)) {
-                sender.sendMessage(ChatColor.RED + "Key " + ChatColor.DARK_AQUA + key + ChatColor.RED + " does not exist.");
-                return;
-            }
-            sender.sendMessage(ChatColor.DARK_AQUA + key + ChatColor.WHITE + ": " + ChatColor.DARK_GREEN + settings.get(key));
-            return;
-        }
-        if (args[1].equalsIgnoreCase("set")) {
-            String key = args[2];
-
-            if (!settings.contains(key)) {
-                sender.sendMessage(ChatColor.RED + "Key " + ChatColor.DARK_AQUA + key + ChatColor.RED + " does not exist.");
-                return;
-            }
-
-            String value = args[3];
-
-            plugin.changeSetting(settings, key, value);
-
-            sender.sendMessage(ChatColor.GREEN + "Successfully set " + ChatColor.DARK_AQUA + key + " to " + ChatColor.DARK_GREEN + value);
-            return;
-        }
-        // if none of the above commands were correct:
-        sender.sendMessage(ChatColor.RED + "Invalid command.");
     }
 }
---
+++ src/main/java/me/stahu/gsblockshuffle/commands/subcommands/DebugSubcommand.java
@@ -0,0 +1,115 @@
+package me.stahu.gsblockshuffle.commands.subcommands;
+
+import me.stahu.gsblockshuffle.GSBlockShuffle;
+import me.stahu.gsblockshuffle.event.GameStateManager;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.util.HashSet;
+
+public class DebugSubcommand {
+    private final GSBlockShuffle plugin;
+    private final GameStateManager gameStateManager;
+    private final YamlConfiguration settings;
+    private final HashSet<CommandSender> debugModeUsers = new HashSet<>();
+
+    public DebugSubcommand(GSBlockShuffle plugin, GameStateManager gameStateManager, YamlConfiguration settings) {
+        this.plugin = plugin;
+        this.gameStateManager = gameStateManager;
+        this.settings = settings;
+    }
+
+    public void parseSubcommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!sender.hasPermission("gsblockshuffle.command.debug")) {
+            sender.sendMessage(ChatColor.RED + "You do not have permission to execute this command.");
+            return;
+        }
+
+        if (!debugModeUsers.contains(sender)) {
+            debugModeUsers.add(sender);
+            sender.sendMessage(ChatColor.RED + "WARNING! Debug mode may cause the plugin to behave non-deterministically.");
+        }
+
+        if (args.length == 1) {
+            showOptions(sender);
+            return;
+        }
+
+        switch (args[1].toLowerCase()) {
+            case "startgame" -> startGame(sender);
+            case "newround" -> newRound(sender);
+            case "endround" -> endRound(sender);
+            case "endgame" -> endGame(sender);
+            case "getroundsremaining" -> getRoundsRemaining(sender);
+            case "get" -> getSetting(sender, args);
+            case "set" -> setSetting(sender, args);
+            case "savesettings" -> saveSettings(sender);
+        }
+    }
+
+    private void showOptions(CommandSender sender) {
+        for (String key : settings.getKeys(false)) {
+            sender.sendMessage(ChatColor.DARK_AQUA + key + ChatColor.WHITE + ": " + ChatColor.DARK_GREEN + settings.get(key));
+        }
+    }
+
+    private void startGame(CommandSender sender) {
+        sender.sendMessage(ChatColor.GREEN + "Game start invoked.");
+        gameStateManager.setGameState(1);
+    }
+
+    private void newRound(CommandSender sender) {
+        sender.sendMessage(ChatColor.GREEN + "New round invoked.");
+        gameStateManager.newRound();
+    }
+
+    private void endRound(CommandSender sender) {
+        sender.sendMessage(ChatColor.GREEN + "End round invoked.");
+        gameStateManager.endRound();
+    }
+
+    private void endGame(CommandSender sender) {
+        sender.sendMessage(ChatColor.GREEN + "End game invoked.");
+        gameStateManager.setGameState(0);
+    }
+
+    private void getRoundsRemaining(CommandSender sender) {
+        sender.sendMessage(ChatColor.GREEN + "Rounds remaining: " + ChatColor.DARK_AQUA + gameStateManager.getRoundsRemaining());
+    }
+
+    private void getSetting(CommandSender sender, String[] args) {
+        String key = args[2];
+        if (!settings.contains(key)) {
+            sender.sendMessage(ChatColor.RED + "Key " + ChatColor.DARK_AQUA + key + ChatColor.RED + " does not exist.");
+            return;
+        }
+        sender.sendMessage(ChatColor.DARK_AQUA + key + ChatColor.WHITE + ": " + ChatColor.DARK_GREEN + settings.get(key));
+    }
+
+    private void setSetting(CommandSender sender, String[] args) {
+        String key = args[2];
+
+        if (!settings.contains(key)) {
+            sender.sendMessage(ChatColor.RED + "Key " + ChatColor.DARK_AQUA + key + ChatColor.RED + " does not exist.");
+            return;
+        }
+
+        String value = args[3];
+
+        plugin.changeSetting(settings, key, value);
+
+        sender.sendMessage(ChatColor.GREEN + "Successfully set " + ChatColor.DARK_AQUA + key + " to " + ChatColor.DARK_GREEN + value);
+    }
+
+    private void saveSettings(CommandSender sender) {
+        try {
+            plugin.saveSettings(settings);
+            sender.sendMessage(ChatColor.GREEN + "Successfully saved settings.");
+        } catch (Exception e) {
+            System.out.println(e);
+            sender.sendMessage(ChatColor.RED + "Failed to save settings.");
+        }
+    }
+}
