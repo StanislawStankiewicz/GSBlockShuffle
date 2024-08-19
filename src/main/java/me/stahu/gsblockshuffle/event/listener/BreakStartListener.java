0aea7324699d831a61b135836ee3e3a11636a451
KieVitz <s217442@sggw.edu.pl>
2024-08-19 15:13:12 +0200

    Rename break events to be less repetitive and more descriptive

--- src/main/java/me/stahu/gsblockshuffle/event/listener/BreakStartListener.java
+++ src/main/java/me/stahu/gsblockshuffle/event/listener/BreakStartListener.java
@@ -2,10 +2,14 @@ package me.stahu.gsblockshuffle.event.listener;
 
 import me.stahu.gsblockshuffle.event.GameEventListener;
 import me.stahu.gsblockshuffle.event.type.RoundBreakEvent;
+import me.stahu.gsblockshuffle.event.type.BreakStartEvent;
+
+public class BreakStartListener implements GameEventListener<BreakStartEvent> {
 
 public class RoundBreakListener implements GameEventListener<RoundBreakEvent> {
     @Override
     public void onGameEvent(RoundBreakEvent event) {
         // currently no usage
+    public void onGameEvent(BreakStartEvent event) {
     }
 }
--- src/main/java/me/stahu/gsblockshuffle/event/type/BreakEndEvent.java
+++ src/main/java/me/stahu/gsblockshuffle/event/type/BreakEndEvent.java
@@ -2,5 +2,5 @@ package me.stahu.gsblockshuffle.event.type;
 
 import me.stahu.gsblockshuffle.event.GameEvent;
 
-public record RoundBreakEvent() implements GameEvent {
+public record BreakEndEvent() implements GameEvent {
 }
